2c3cfd0fcb77bbdde51a0ea3a20fbe15e46433f7
Jen Garcia <jen@haverstack.com>
2021-08-10 19:53:50 -0400

    Boilerplate for Preact, Tailwind, Jest, ESLint, and Parcel

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # umpapa
+
+An app for creating avatars. A similar idea to [Picrew](https://picrew.me) but a little simpler and with the ability to draw character parts directly in the app.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+export default () => {
+  return (
+    <div class="bg-yellow-100 p-4 h-screen">
+      <div class="bg-white container rounded-sm mx-auto p-4">
+        Hello, world!
+      </div>
+    </div>
+  );
+};

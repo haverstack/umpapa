c5be290e5708b3361c7f75bd7229ef0ba402c2f7
Jen Garcia <jen@haverstack.com>
2021-10-24 19:24:33 -0400

    Display a message in the app that is fetched from the api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,19 @@
-import { handleRequest } from './handler';
+import { corsHeaders, handleOptions } from './cors';
+
+const withStatus = (status: number = 200, headers?: {[key: string]: string}) => ({
+  status,
+  headers: { ...corsHeaders, ...(headers || {}) }
+});
+
+const handleRequest = async (request: Request): Promise<Response> => {
+  return new Response(`request method: ${request.method}`, withStatus(200));
+};
 
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request));
+  if (event.request.method === 'OPTIONS') {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(event.request));
+  } else {
+    event.respondWith(handleRequest(event.request));
+  }
 });
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,25 @@
 import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+
+const BASE_URL = process.env.NODE_ENV == "production" ? "https://umpapa-api.haverstack.workers.dev" : "http://localhost:8787";
 
 const App = () => {
+  const [message, setMessage] = useState("Loading...");
+
+  const getMessage = async () => {
+    const resp = await fetch(
+      BASE_URL
+    );
+    setMessage(await resp.text());
+  };
+
+  useEffect(() => {
+    getMessage();
+  }, []);
+
   return (
-    <div class="bg-yellow-100 p-4 h-screen">
-      <div class="bg-white container rounded-sm mx-auto p-4">Hello, world!</div>
+    <div class="bg-red-100 p-4 h-screen">
+      <div class="bg-white container rounded-sm mx-auto p-4">{ message }</div>
     </div>
   );
 };
